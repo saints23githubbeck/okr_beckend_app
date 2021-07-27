474b5a835d167148971894350274719db3282190
Patrick <patrick01.programmer@gmail.com>
2021-07-27 10:12:35 +0000

    Made a little changes to server.js

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const { sequelize } = require("./models");
 const db = require("./models");
 const http = require("http");
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
